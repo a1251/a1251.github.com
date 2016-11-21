8825fecdffa142f834b78d931d63ab24ee2d2b63
a1251 <1446736401@qq.com>
2016-11-22 01:48:54 +0800

    11.22凌晨

---
+++ jslib/myhomepage.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $("textarea").attr("disabled","disabled");
+    // var value = $("table>tr>td>input").slider({
+    //     value: 100,
+    //     min: 0,
+    //     max: 500,
+    //     step: 50
+    // });
+    // var shulian = $(".shulian");
+    // $("table>tr>td>input").change(function () {
+    //     if (value<25){
+    //         var val = '掌握';
+    //     }else if (value<50){
+    //         var val = '良好';
+    //     }else if (value<75){
+    //         var val = '熟练';
+    //     }else if (value<=100){
+    //         var val = '精通';
+    //     }
+    //     shulian.html(val);
+    // });
+
+
+});
