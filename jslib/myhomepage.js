c223689ca133a4d6a87a47280be1b3b0e3e9828b
a1251 <1446736401@qq.com>
2016-11-22 23:03:12 +0800

    2016.11.22-23:03

--- jslib/myhomepage.js
+++ jslib/myhomepage.js
@@ -11,25 +11,22 @@ $(document).ready(function () {
             top: '-250px'
         });
     });
-    // var value = $("table>tr>td>input").slider({
-    //     value: 100,
-    //     min: 0,
-    //     max: 500,
-    //     step: 50
-    // });
-    // var shulian = $(".shulian");
-    // $("table>tr>td>input").change(function () {
-    //     if (value<25){
-    //         var val = '掌握';
-    //     }else if (value<50){
-    //         var val = '良好';
-    //     }else if (value<75){
-    //         var val = '熟练';
-    //     }else if (value<=100){
-    //         var val = '精通';
-    //     }
-    //     shulian.html(val);
-    // });
+    var sliders = $("table tr td input[type=range]");
+    var shulians = $(".shulian");
+    sliders.change(function () {
+        var value = $(this).val();
+        // alert(value);
+        if (value<25){
+            var val = '掌握';
+        }else if (value<50){
+            var val = '良好';
+        }else if (value<75){
+            var val = '熟练';
+        }else if (value<=100){
+            var val = '精通';
+        }
+        $(this).next().html(val);
+    });
 
 
 });
